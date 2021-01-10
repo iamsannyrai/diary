cdbba6219d26921b7fe0c61099cd5898d146c36e
office <iam.sannyrai@gmail.com>
2021-01-10 21:11:36 +0545

    refactor

--- diary/resources/user.py
+++ diary/resources/user.py
@@ -2,14 +2,14 @@ from flask_restful import Resource
 from diary.models.user import User, UserSchema
 
 
-all_user_schema = UserSchema(many=True)
+users_schema = UserSchema(many=True)
 user_schema = UserSchema()
 
 
 class UserResource(Resource):
     def get(self):
         all_users = User.query.all()
-        return all_user_schema.dump(all_users)
+        return users_schema.dump(all_users)
 
 
 class SingleUserResource(Resource):
